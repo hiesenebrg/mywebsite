f86f1eb43d9167063214c8bf1f09364ed136be40
hiesenebrg <adarshsingh8008@gmail.com>
2022-09-18 23:05:09 +0530

    now we are using passport as a middleware to authenicate

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,17 @@
 const express = require('express')
 const router = express.Router()
 
+const passport = require('passport');
 const controller = require('../controllers/homecontrollers')
 router.get('/sign-up' , controller.signup);
 router.post('/create' , controller.create);
 router.get('/sign-in' , controller.signin);
 router.get('/profilepage' , controller.profilepage);
-router.post('/create-session' , controller.createsession);
+// now we are using passport as a middleware to authenicate
+router.post('/create-session' , passport.authenticate(
+'local',
+{failureRedirect : '/users/sign-in'},
+),controller.createsession);
 
 
 module.exports = router;
