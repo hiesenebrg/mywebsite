130bf0bb14ea5456012309ffcb320c46b01f6561
hiesenebrg <adarshsingh8008@gmail.com>
2022-09-18 20:42:22 +0530

    cookie-parser installed and app.use(cookie-parser) created

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
-
+const cookieparser = require('cookie-parser');
 const port = 8000;
 const mongoose = require("mongoose");
 const app = express();
 const db = require('./config/mongoose');
 
 app.use(express.urlencoded());
-
+app.use(cookieparser);
 app.use('/', require('./routes'));
 app.set('view engine','ejs');
 app.set('views','./views');
