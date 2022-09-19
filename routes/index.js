6cfdac3987e46d99cf7ade7bd9db7395baef5fc6
hiesenebrg <adarshsingh8008@gmail.com>
2022-09-19 19:28:01 +0530

    redirected to posts from index in route folder

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const controller = require('../controllers/homecontrollers');
 router.get('/',controller.home);
 router.use('/users',require('./users'));
+router.use('/posts',require('./posts'));
 
 module.exports = router;
