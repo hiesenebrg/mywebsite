3a76d85edbcbe1415c643cd2c3ed6b2edb6b8bfd
hiesenebrg <adarshsingh8008@gmail.com>
2022-09-19 22:17:41 +0530

    comment Controller created

---
+++ controllers/commentcontoller.js
@@ -0,0 +1,23 @@
+const Comment = require('../models/comment');
+const Post = require('../models/post');
+module.exports.create = function(req,res){
+    // now here req.body.post has the name set in comment form which has value post._id becvause we are checking that user must comment on the valid post
+    Post.findById(req.body.post,function(err,post){
+        if(post){
+            Comment.create({
+                content:req.body.content,
+                post: req.body.post,
+                user: req.user._id
+            },function(err,comment){
+                if(err){
+                    console.log("there is an error while creating the error",err);
+                }
+                // now we have to update the commen in the post collection that's why we have changes the post model 
+                post.commnents.push(comment);
+                // remember to save the post while updating
+                post.save();
+            });
+        }
+    });
+
+    }
