876bc330a7036c1d7225c44664df3d03f1ecf8fb
hiesenebrg <adarshsingh8008@gmail.com>
2022-09-19 19:55:21 +0530

    some error fixed

--- controllers/postcontroller.js
+++ controllers/postcontroller.js
@@ -1,13 +1,17 @@
 const post = require('../models/post')
 module.exports.create = function(req, res){
+    
 post.create({
-    content : req.body.post,
+   
+    content : req.body.content,
     // this req.user constains the data of the current signed in user and it gets due to passport-local-strategy as discussed earlier
     user : req.user._id
 },function(err,post){
     if(err){
+        
         console.log("Error while dreating the post");
     }
+    
     return res.redirect('back');
 })
 }
