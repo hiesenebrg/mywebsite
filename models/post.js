5fc2d12c866f07c0982f9bc5adca3021ef190747
hiesenebrg <adarshsingh8008@gmail.com>
2022-09-19 21:42:03 +0530

    include the array of ids of all the comment in this post schema itself

--- models/post.js
+++ models/post.js
@@ -9,7 +9,12 @@ const postSchema = new mongoose.Schema({
         // this srefers to user id
         type: mongoose.Schema.Types.ObjectId,
         ref:'User'
-    }
+    },
+    // include the array of ids of all the comment in this post schema itself
+    comments :[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'comment'
+    }]
     },{
         timestamps: true
     
