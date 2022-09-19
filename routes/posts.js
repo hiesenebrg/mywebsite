ccd58e90a5b2de0e8c5802b4278106aa62f169d1
hiesenebrg <adarshsingh8008@gmail.com>
2022-09-19 19:27:14 +0530

    router created for post

---
+++ routes/posts.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const postcontroller = require('../controllers/postcontroller');
+
+router.post('/create', postcontroller.post);
+
+
+module.exports = router;
