dede69900e89e7a45928d0a3dacbaffa30c4b3a7
hiesenebrg <adarshsingh8008@gmail.com>
2022-10-10 21:56:37 +0530

    development created

--- config/enviornment.js
+++ config/enviornment.js
@@ -2,15 +2,15 @@ const development ={
     name: 'development',
     assets_path : './assets',
     session_cookie :'blahsomething',
-    db:'mywebsite_dbb',
+    db:"mywebsite_dbb",
     smtp :{
         service: 'gmail',
         host: 'smtp.gmail.com',
         port: 587,
         secure: false,
         auth: {
-            user: 'adarshsingh8008@gmail.com',
-            pass:'tpvjxaqkopvkkztu'
+            user: "adarshsingh8008@gmail.com",
+            pass:"tpvjxaqkopvkkztu"
             // pass: 'udetyurctriwsqwh'
         }
     },
@@ -20,6 +20,24 @@ const development ={
     jwt_secret:'codial'
 }
 const production ={
-    name: 'production'
+    name: 'production',
+    assets_path : "./assets",
+    session_cookie :process.env.MYWEBSITE_SESSION_COOKIE,
+    db:process.env.MYWEBSITE_DB,
+    smtp :{
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.MYWEBSITE_USERNAME,
+            pass:process.env. MYWEBSITE_PASSWORD
+            // pass: 'udetyurctriwsqwh'
+        }
+    },
+    google_client_id : process.env.MYWEBSITE_GOOGLE_CLIENTID,
+    google_client_secret :process.env.MYWEBSITE_GOOGLE_SECRET,
+    google_callback_url : process.env. MYWEBSITE_CALLBACK_URL, 
+    jwt_secret:process.env.MYWEBSITE_JWT_SECRET
 }
-module.exports = development; 
+module.exports = eval(process.env.MYWEBSITE_CODIAL) == undefined ? development :eval(process.env.MYWEBSITE_CODIAL); 
